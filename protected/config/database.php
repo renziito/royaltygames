1683ab747c6c02f29665d1d146af1ae214322b33
Renziito <sepia.aki@gmail.com>
2020-04-20 02:49:33 -0500

    databse

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,20 +1,10 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] == "localhost") {
-    $config = array(
-        'connectionString' => 'mysql:host=localhost;dbname=db_royaltygames',
-        'emulatePrepare' => true,
-        'username' => 'royaltyUser',
-        'password' => 'us3rp4ssw0rd',
-        'charset' => 'utf8',
-    );
-} else {
-    $config = array(
-        'connectionString' => 'mysql:host=localhost;dbname=jackpot',
-        'emulatePrepare' => true,
-        'username' => 'username',
-        'password' => 'wordPa$$1',
-        'charset' => 'utf8',
-    );
-}
+$config = array(
+    'connectionString' => 'mysql:host=localhost;dbname=jackpot',
+    'emulatePrepare' => true,
+    'username' => 'username',
+    'password' => 'wordPa$$1',
+    'charset' => 'utf8',
+);
 return $config;
