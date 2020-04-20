6bdb23ec422ff7d02cf1d44b5bc84ecf012c0b00
Renziito <sepia.aki@gmail.com>
2020-04-20 02:39:04 -0500

    database

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,10 +1,20 @@
 <?php
 
-// This is the database connection configuration.
-return array(
-    'connectionString' => 'mysql:host=localhost;dbname=db_royaltygames',
-    'emulatePrepare' => true,
-    'username' => 'royaltyUser',
-    'password' => 'us3rp4ssw0rd',
-    'charset' => 'utf8',
-);
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+    $config = array(
+        'connectionString' => 'mysql:host=localhost;dbname=db_royaltygames',
+        'emulatePrepare' => true,
+        'username' => 'royaltyUser',
+        'password' => 'us3rp4ssw0rd',
+        'charset' => 'utf8',
+    );
+} else {
+    $config = array(
+        'connectionString' => 'mysql:host=localhost;dbname=jackpot',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => 'xH$c9*M6sfAh',
+        'charset' => 'utf8',
+    );
+}
+return $config;
